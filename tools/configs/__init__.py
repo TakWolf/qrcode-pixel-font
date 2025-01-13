c488f470cf2f5ee793b0e56092313924c0da1435
TakWolf <takwolf@foxmail.com>
2025-01-13 19:56:01 +0800

    代码整理

--- tools/configs/__init__.py
+++ tools/configs/__init__.py
@@ -1,7 +1,5 @@
-from typing import Literal, get_args
 
 version = '0.0.0'
 version_time = '2025-01-01'
 
-type FontFormat = Literal['otf', 'ttf', 'woff2', 'bdf', 'pcf']
-font_formats = list[FontFormat](get_args(FontFormat.__value__))
+font_formats = ['otf', 'ttf', 'woff2', 'bdf', 'pcf']
