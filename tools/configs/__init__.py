17b5815c092c654905c96307296cfe91e5650f8b
TakWolf <takwolf@foxmail.com>
2025-01-14 00:40:45 +0800

    1.1.0

--- tools/configs/__init__.py
+++ tools/configs/__init__.py
@@ -1,7 +1,7 @@
 from typing import Literal, get_args
 
-version = '1.0.0'
-version_time = '2025-01-13'
+version = '1.1.0'
+version_time = '2025-01-14'
 
 type FontFormat = Literal['otf', 'ttf', 'woff2', 'bdf', 'pcf']
 font_formats = list[FontFormat](get_args(FontFormat.__value__))
